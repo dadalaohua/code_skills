40cd24af425c36ff67c187add5837a8181e950b7
root <root@Linux-host.(none)>
2020-07-24 22:51:04 +0800

    add bits

---
+++ basic/0.基础知识/4.检测有多少个bit被置位/2.字节/test_code.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+const int ones[] = {0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 1, 2, 2,\
+ 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4,\
+ 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 1, 2, 2, 3, 2,\
+ 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5,\
+ 5, 6, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5,\
+ 6, 4, 5, 5, 6, 5, 6, 6, 7, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,\
+ 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 2, 3, 3, 4, 3, 4, 4, 5, 3,\
+ 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 2, 3,\
+ 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5,\
+ 6, 5, 6, 6, 7, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 4, 5, 5, 6,\
+ 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8};
+ 
+unsigned int count_bit1_1(unsigned char data)
+{
+    unsigned int count = 0;     //置0计数
+    
+    while(data)
+    {
+       count += (data&1);   //检查Num最后一位是否为1
+       data >>= 1;          //位移一位
+    }
+    
+    return count;
+}
+
+unsigned int count_bit1_2(unsigned char data)
+{
+    unsigned int count = 0;     //置0计数
+    
+    //一个8位的数减1相当于把从后数第一个1及之后的数取反，
+    //然后与原数相与就把这些去反以后的位清0了，
+    //等到所有的位都被清0，就会跳出循环。
+    //这种算法的效率跟1的个数有关，有多少1就循环多少次。
+    while(data)
+    {
+       data &= (data - 1);
+       count ++;
+    }
+    
+    return count;
+}
+
+unsigned int count_bit1_3(unsigned char data)
+{
+    data = (data & 0x55) + ((data >> 1) & 0x55);
+    data = (data & 0x33) + ((data >> 2) & 0x33);
+    data = (data & 0x0f) + ((data >> 4) & 0x0f);
+    
+    return data;
+}
+
+unsigned int count_bit1_4(unsigned char data)
+{
+    return (ones[data & 0xff]);
+
+    return data;
+}
+
+int main(int argc, char* argv[])
+{ 
+    unsigned char test = 0x5F;
+
+    printf("count_bit1: %d\n", count_bit1_1(test));
+    printf("count_bit1: %d\n", count_bit1_2(test));
+    printf("count_bit1: %d\n", count_bit1_3(test));
+    printf("count_bit1: %d\n", count_bit1_4(test));
+
+    return 0;
+}
